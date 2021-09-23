e4412d771eb3bfffb9c0590be60873a3e00d4d6e
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2021-09-23 00:33:40 -0300

    fix: remove home

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './app.css';
 
 const App = () => {
 
-  const HomePage = React.lazy(() => import("HomeApp/HomePage"));
+  // const HomePage = React.lazy(() => import("HomeApp/HomePage"));
   const ContactPage = React.lazy(() => import("ContactApp/ContactPage"));
   window.testeInfo = "testeInfo";
 
@@ -23,9 +23,12 @@ const App = () => {
         </Navbar>
         <Switch>
           <Route exact path="/">
-            <Suspense fallback={<div>Loading...</div>}>
+            {/* <Suspense fallback={<div>Loading...</div>}>
               <HomePage />
-            </Suspense>
+            </Suspense> */}
+            <div>
+              Hello
+            </div>
           </Route>
           <Route exact path="/contact">
             <Suspense fallback={<div>Loading...</div>}>
